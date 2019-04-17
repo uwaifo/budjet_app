8b2780997a0abfd86ab7d4c5829b20e8d4eb6085
uwaifo <mr.uwaifo@gmail.com>
2019-04-17 09:43:24 +0100

    Setting up firstevent listeners

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-console.log("ok");
-var budjetController = (function() {
-  var age = 4;
-  var newAge = function(addAge) {
-    return age + addAge;
-  };
-
-  return {
-    publicTest: function(arg) {
-      return newAge(arg);
-    }
-  };
-})();
+// budjet conrolleer
+var budjetController = (function() {})();
 
 let UIController = (function() {})();
 
+// Global app controller
 let controller = (function(budgetCtrl, UICtrl) {
-  var x = budgetCtrl.publicTest(44);
-  return {
-    publicMethod: function() {
-      console.log(x);
-    }
+  let ctrlAddItem = function() {
+    console.log("Adding gratually  . . . ");
+    //Get th field input
+    //Add th item  to budget controller
+    //Add the item to the UI
+    //Calculate the budget
+    //Display the budjet  in the UI
   };
-  /// CODES TO BE ADDED
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    //console.log(event);
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budjetController, UIController);
