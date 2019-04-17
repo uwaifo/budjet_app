90e241566ea887fc5f1faf2a76a18f84418974bb
uwaifo <mr.uwaifo@gmail.com>
2019-04-17 12:45:03 +0100

    Reading Input Data

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-console.log("ok");
-var budjetController = (function() {
-  var age = 4;
-  var newAge = function(addAge) {
-    return age + addAge;
-  };
+// budjet conrolleer
+var budjetController = (function() {})();
 
+let UIController = (function() {
+  let DOMStrings = {
+    inputTypes: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
   return {
-    publicTest: function(arg) {
-      return newAge(arg);
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputTypes).value,
+        description: document.querySelector(DOMStrings.inputDesc).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
     }
   };
 })();
 
-let UIController = (function() {})();
-
+// Global app controller
 let controller = (function(budgetCtrl, UICtrl) {
-  var x = budgetCtrl.publicTest(44);
-  return {
-    publicMethod: function() {
-      console.log(x);
-    }
+  let DOM = UICtrl.getDOMStrings();
+  let ctrlAddItem = function() {
+    //console.log("Adding gratually  . . . ");
+    //1.Get th field input
+    let input = UICtrl.getInput();
+    console.log(input);
+    //Add th item  to budget controller
+    //Add the item to the UI
+    //Calculate the budget
+    //Display the budjet  in the UI
   };
-  /// CODES TO BE ADDED
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    //console.log(event);
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budjetController, UIController);
